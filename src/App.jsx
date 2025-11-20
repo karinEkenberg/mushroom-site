1e05a321e3579267cc1cda2b9b4a49130e8a8ac5
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-11-20 13:40:27 +0100

    refactor: update index.html and App.jsx for Tailwind integration; remove unused assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-[url('./images/lovet.png')] bg-charcoal bg-repeat bg-[length:300px] h-96 w-full flex items-center">
+        <div className="w-full lg:w-fit mx-auto bg-canvas text-charcoal p-8 rounded-lg">
+          <h1 className="text-3xl font-bold text-olive">Karins Svampar</h1>
+        </div>
       </div>
-      <h1 className="text-3xl font-bold text-blue-300">Karins Svampar</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
