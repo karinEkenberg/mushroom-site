6f4d33722177686b3d764bb5966927b474e74f32
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-11-21 13:12:57 +0100

    fix: correct casing of MainSections component import and usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./App.css";
-import Mainsections from "./components/mainsections";
+import  MainSections from "./components/MainSections.jsx";
 function App() {
   return (
     <>
       <div className="bg-[url('./src/assets/images/background.png')] bg-charcoal bg-repeat bg-[length:150px] h-96 w-full items-center">
-        <Mainsections>
+        <MainSections>
           <h1 className="text-3xl font-bold text-olive text-center">
             Karins Svampar
           </h1>
-        </Mainsections>
-        <Mainsections>
+        </MainSections>
+        <MainSections>
           <h2 className="text-3xl font-bold text-olive text-center">
             Karins Svampar
           </h2>
-        </Mainsections>
+        </MainSections>
       </div>
     </>
   );
--- src/components/Mainsections.jsx
+++ src/components/Mainsections.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Mainsections extends Component {
+export default class MainSections extends Component {
   render() {
     return (
       <div className="w-[90%] md:w-[80%] lg:max-w-6xl mx-auto bg-canvas text-charcoal p-8 rounded-lg shadow-lg my-12">
