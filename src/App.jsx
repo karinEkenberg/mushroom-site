630a12e06a07f7a0af57cbff232d1a6a5d6a9ae5
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-11-21 13:30:15 +0100

    feat: update App component layout with background image and additional MainSections

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import "./App.css";
-import  MainSections from "./components/MainSections.jsx";
+import MainSections from "./components/MainSections.jsx";
+import bgImage from "./assets/images/background.png";
 function App() {
   return (
     <>
-      <div className="bg-[url('./src/assets/images/background.png')] bg-charcoal bg-repeat bg-[length:150px] h-96 w-full items-center">
-        <MainSections>
-          <h1 className="text-3xl font-bold text-olive text-center">
-            Karins Svampar
-          </h1>
-        </MainSections>
-        <MainSections>
-          <h2 className="text-3xl font-bold text-olive text-center">
-            Karins Svampar
-          </h2>
-        </MainSections>
-      </div>
+      <div
+        style={{ backgroundImage: `url(${bgImage})` }}
+        className="bg-charcoal fixed -z-10 bg-repeat bg-[length:150px] h-full top-0 w-full items-center"
+      ></div>
+      <MainSections>
+        <h1 className="text-3xl font-bold text-olive text-center">
+          Karins Svampar
+        </h1>
+      </MainSections>
+      <MainSections>
+        <h2 className="text-3xl font-bold text-olive text-center">
+          Karins Svampar
+        </h2>
+      </MainSections>
+      <MainSections>
+        <h2 className="text-3xl font-bold text-olive text-center">
+          Karins Svampar
+        </h2>
+      </MainSections>
+      <MainSections>
+        <h2 className="text-3xl font-bold text-olive text-center">
+          Karins Svampar
+        </h2>
+      </MainSections>
     </>
   );
 }
