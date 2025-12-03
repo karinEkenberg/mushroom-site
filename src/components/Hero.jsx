41f924d3da42ce78d4aaefa7c603b90de9a17afc
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-12-03 15:06:07 +0100

    feat: update Hero component layout, enhance heading styles, and improve image handling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,25 +6,31 @@ const Hero = () => {
   return (
     <MainSections>
       <div className="relative mx-auto pb-5 pt-5 w-[90%]">
-        <h1 className="text-3xl font-bold text-olive text-center">
-          Karins Svampar
-        </h1>
         <picture className="w-full block">
           <source
             media="(max-width: 768px)"
             srcSet="./src/assets/images/svampsidan-hero-mobile.webp"
           />
+
           <img
             src="./src/assets/images/svampsidan-hero.webp"
             alt="svampsidan-hero"
             loading="lazy"
-            className="w-full h-[650px] object-cover"
+            className="w-full object-cover"
           />
         </picture>
 
-        <div className="absolute inset-0 flex flex-col sm:flex-row md:items-end items-center md:justify-start justify-end md:gap-7 md:p-20 gap-4 p-10 ">
-          <Buttons>Utforska Nu</Buttons>
-          <Buttons>Lär Dig Mer</Buttons>
+        <div className="absolute inset-0 flex flex-col justify-between p-4">
+          <div className="w-full flex justify-center pt-20">
+            <h1 className="text-5xl md:text-7xl text-white [-webkit-text-stroke:1px_black] font-delius text-center">
+              Karins Svampar
+            </h1>
+          </div>
+
+          <div className="flex flex-col sm:flex-row items-center md:justify-start justify-end gap-4 w-full pb-4 md:pl-4">
+            <Buttons>Utforska Nu</Buttons>
+            <Buttons>Lär Dig Mer</Buttons>
+          </div>
         </div>
       </div>
     </MainSections>
