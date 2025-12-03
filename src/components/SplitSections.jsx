09284178a54185fc9d6723e3e36f155acd73fa79
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-12-03 16:42:43 +0100

    fix: adjust section width and margin for better layout in SplitSections component

--- src/components/SplitSections.jsx
+++ src/components/SplitSections.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SplitSections({ children }) {
   return (
-    <section className="flex flex-col gap-8 md:flex-row justify-between items-center gap-8 my-12">
+    <section className="flex flex-col md:w-[90%] md:mx-auto gap-8 md:flex-row justify-between items-center gap-8 my-12">
       {children}
     </section>
   );
