81b64768b8d042c271daff4fb90c55e5fecdf529
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-12-03 14:42:46 +0100

    feat: enhance Hero component layout, add Delius font, and improve button styles

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -3,9 +3,11 @@ import React, { Component } from "react";
 export default class Buttons extends Component {
   render() {
     return (
-      <button className="bg-primary-normal hover:bg-primary-hovering 
+      <button
+        className="bg-primary-normal hover:bg-primary-hovering 
       text-charcoal active:bg-primary-active disabled:bg-primary-disabled 
-      focus:outline-dashed bg-center font-bold py-2 px-4 rounded">
+      focus:outline-dashed bg-center font-bold py-2 px-4 rounded shadow-lg"
+      >
         {this.props.children}
       </button>
     );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,30 +2,33 @@ import React, { Component } from "react";
 import MainSections from "./MainSections";
 import Buttons from "./Buttons";
 
-export default class Hero extends Component {
-  render() {
-    return (
-      <MainSections>
-        <div className="relative w-full">
-          <picture className="w-full block">
-            <source
-              media="(max-width: 768px)"
-              srcSet="./src/assets/images/svampsidan-hero-mobile.webp"
-            />
-            <img
-              src="./src/assets/images/svampsidan-hero.webp"
-              alt="svampsidan-hero"
-              loading="lazy"
-              className="w-full h-[650px] object-cover"
-            />
-          </picture>
+const Hero = () => {
+  return (
+    <MainSections>
+      <div className="relative mx-auto pb-5 pt-5 w-[90%]">
+        <h1 className="text-3xl font-bold text-olive text-center">
+          Karins Svampar
+        </h1>
+        <picture className="w-full block">
+          <source
+            media="(max-width: 768px)"
+            srcSet="./src/assets/images/svampsidan-hero-mobile.webp"
+          />
+          <img
+            src="./src/assets/images/svampsidan-hero.webp"
+            alt="svampsidan-hero"
+            loading="lazy"
+            className="w-full h-[650px] object-cover"
+          />
+        </picture>
 
-          <div className="absolute inset-0 flex flex-col sm:flex-row md:items-end items-center md:justify-start justify-end gap-4 p-4 bg-black/30">
-            <Buttons>Utforska Nu</Buttons>
-            <Buttons>Lär Dig Mer</Buttons>
-          </div>
+        <div className="absolute inset-0 flex flex-col sm:flex-row md:items-end items-center md:justify-start justify-end md:gap-7 md:p-20 gap-4 p-10 ">
+          <Buttons>Utforska Nu</Buttons>
+          <Buttons>Lär Dig Mer</Buttons>
         </div>
-      </MainSections>
-    );
-  }
-}
+      </div>
+    </MainSections>
+  );
+};
+
+export default Hero;
