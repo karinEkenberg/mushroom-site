577538f6c67a7cb5c906c0eb413541c25c8013da
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-12-04 11:17:17 +0100

    fix: adjust button styles for improved layout and responsiveness

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -5,8 +5,8 @@ export default class Buttons extends Component {
     return (
       <button
         className="bg-primary-normal hover:bg-primary-hovering 
-      text-charcoal active:bg-primary-active disabled:bg-primary-disabled 
-      focus:outline-dashed bg-center font-bold py-2 px-4 rounded shadow-lg"
+      text-charcoal w-full py-2 px-2 text-lg active:bg-primary-active disabled:bg-primary-disabled 
+      focus:outline-dashed bg-center font-bold md:w-auto md:py-2 md:px-4 rounded shadow-lg"
       >
         {this.props.children}
       </button>
