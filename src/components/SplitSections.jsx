85810f67bee4b8e85bdcda04876e83e5bab4e55d
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-12-04 11:11:35 +0100

    fix: update layout from flex to grid in SplitSections component for improved responsiveness

--- src/components/SplitSections.jsx
+++ src/components/SplitSections.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SplitSections({ children }) {
   return (
-    <section className="flex flex-col md:w-[90%] md:mx-auto gap-8 md:flex-row justify-between items-center gap-8 my-12">
+    <section className="grid grid-cols-1 md:grid-cols-2 md:w-[90%] md:mx-auto gap-8 items-center my-12">
       {children}
     </section>
   );
